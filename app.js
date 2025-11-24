ac7e409977f4451fee845aee4c2714f5f369dee4
yanyinnicolewu <nicolewu288@gmail.com>
2025-11-24 12:31:55 -0500

    cxz

---
+++ app.js
@@ -0,0 +1,85 @@
+const disneymovie = [
+  {
+    name: "",
+    image: "",
+    alt: "",
+  },
+];
+
+const disneymovie = [
+  {
+    name: "The Lion King",
+    image: "https://link-to-lion-king-image.jpg",
+    alt: "The Lion King movie poster",
+  },
+  {
+    name: "Beauty and the Beast",
+    image: "https://link-to-beauty-and-the-beast-image.jpg",
+    alt: "Beauty and the Beast movie poster",
+  },
+  {
+    name: "Aladdin",
+    image: "https://link-to-aladdin-image.jpg",
+    alt: "Aladdin movie poster",
+  },
+  {
+    name: "Frozen",
+    image: "https://link-to-frozen-image.jpg",
+    alt: "Frozen movie poster",
+  },
+  {
+    name: "Moana",
+    image: "https://link-to-moana-image.jpg",
+    alt: "Moana movie poster",
+  },
+  {
+    name: "Cinderella",
+    image: "https://link-to-cinderella-image.jpg",
+    alt: "Cinderella movie poster",
+  },
+  {
+    name: "Snow White and the Seven Dwarfs",
+    image: "https://link-to-snow-white-image.jpg",
+    alt: "Snow White and the Seven Dwarfs movie poster",
+  },
+  {
+    name: "Peter Pan",
+    image: "https://link-to-peter-pan-image.jpg",
+    alt: "Peter Pan movie poster",
+  },
+  {
+    name: "The Little Mermaid",
+    image: "https://link-to-little-mermaid-image.jpg",
+    alt: "The Little Mermaid movie poster",
+  },
+  {
+    name: "Mulan",
+    image: "https://link-to-mulan-image.jpg",
+    alt: "Mulan movie poster",
+  },
+  {
+    name: "The Jungle Book",
+    image: "https://link-to-jungle-book-image.jpg",
+    alt: "The Jungle Book movie poster",
+  },
+  {
+    name: "Tangled",
+    image: "https://link-to-tangled-image.jpg",
+    alt: "Tangled movie poster",
+  },
+  {
+    name: "Zootopia",
+    image: "https://link-to-zootopia-image.jpg",
+    alt: "Zootopia movie poster",
+  },
+  {
+    name: "Ratatouille",
+    image: "https://link-to-ratatouille-image.jpg",
+    alt: "Ratatouille movie poster",
+  },
+  {
+    name: "Toy Story",
+    image: "https://link-to-toy-story-image.jpg",
+    alt: "Toy Story movie poster",
+  },
+];
