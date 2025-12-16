ca7fce9ca6c71639896d7043f3cfc54b06dfb541
yanyinnicolewu <nicolewu288@gmail.com>
2025-12-16 12:08:40 -0500

    jnjn

--- vite-project/src/index.js
+++ vite-project/src/index.js
@@ -1,16 +1,16 @@
-function removeMovie(event) {
-  event.target.parentElement.remove();
-}
+// function removeMovie(event) {
+//   event.target.parentElement.remove();
+// }
 
-document.getElementById("form").addEventListener("submit", function (e) {
-  e.preventDefualt();
-  let movie = {};
-  movie.title = document.getElementById("title").vaule;
-  movie.url = document.getElementById("url").value;
-  console.log(movie, "TEST");
-  inject(movie);
-  clearFields();
-  document.querySelectorAll(".remove").forEach((button) => {
-    button.addEventListener("clik", removeMovie);
-  });
-});
+// document.getElementById("form").addEventListener("submit", function (e) {
+//   e.preventDefualt();
+//   let movie = {};
+//   movie.title = document.getElementById("title").vaule;
+//   movie.url = document.getElementById("url").value;
+//   console.log(movie, "TEST");
+//   inject(movie);
+//   clearFields();
+//   document.querySelectorAll(".remove").forEach((button) => {
+//     button.addEventListener("clik", removeMovie);
+//   });
+// });
